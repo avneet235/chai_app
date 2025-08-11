e86a20cc1c1007c7e706692557afd7148d5a8417
avneet235 <avneet.dhingra235@gmail.com>
2025-08-11 13:21:44 +0530

    jinja2 and chai app

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home),
     path('about/', views.about),
     path('contact/', views.contact),
+    path('chai/', include('chai.urls')),
 ]
