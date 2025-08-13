8f45cc97287e53011fe6af0248950d30cd161c86
avneet235 <avneet.dhingra235@gmail.com>
2025-08-14 00:07:30 +0530

    ui changes

--- chai/forms.py
+++ chai/forms.py
@@ -2,4 +2,10 @@ from django import forms
 from .models import ChaiVariety
 
 class ChaiVarietyForm(forms.Form):
-  chai_variety = forms.ModelChoiceField(queryset=ChaiVariety.objects.all(), label="Select Chai Variety")
+    chai_variety = forms.ModelChoiceField(
+        queryset=ChaiVariety.objects.all(),
+        label="Select Chai Variety",
+        widget=forms.Select(attrs={
+            'class': 'text-black bg-[#eff6ff] border border-gray-300 rounded-md p-1'
+        })
+    )
